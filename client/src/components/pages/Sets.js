2b4135a43134bdec31d7f201a693dfd2d12836b5
=Tienbom3112000 <nguyenvantien2000113@gmail.com>
2020-11-14 12:19:44 +0700

    Add page Sets

--- client/src/components/pages/Sets.js
+++ client/src/components/pages/Sets.js
@@ -1,10 +1,63 @@
 import React from 'react'
 import { Pane } from 'evergreen-ui'
 import UserHeader from '../UserHeader'
+import { Menu,Heading } from 'evergreen-ui'
+class sets extends React.Component {
+  render() {
+    var listGroups =[
+      { 
+        id : 1,
+        date : "Tháng 10",
+        items : [
+          { 
+            id : 1,
+            amount : "1",
+            title : "Title"
+          },
+          {
+            id : 2,
+            amount : "1",
+            title : "Title"
+          },
+        ]
+      },
+      { 
+        id : 2,
+        date : "Tháng 10",
+        items : [
+          {
+            id : 1,
+            amount : "1",
+            title : "Title"
+          },
+          {
+            id : 2,
+            amount : "1",
+            title : "Title"
+          },
+        ]
+      },
+    ];
 
-export default () => (
-  <Pane>
-    <UserHeader path="/sets" />
-    <p>Sets</p>
+    return (
+      <Pane>
+       <UserHeader path="/sets" />
+        <Menu height={400}>
+          {listGroups.map(group =>(
+            <Menu.Group key = {group.id} date ={group.date}>
+                {group.items.map(item =>(
+                  <Menu.Item >
+                      <Heading size = {400}> {item.amount} </Heading>
+                      <Heading size = {400}> {item.title} </Heading>
+                  </Menu.Item>
+                )
+                )}
+            </Menu.Group>
+          ))}
+       </Menu>
+
   </Pane>
-)
+    )
+  }
+}
+export default sets;
