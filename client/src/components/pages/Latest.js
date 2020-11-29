6f81a21b17318c482ca3261a571090f92b92d5ee
lucasnguyen23hqv <lucasnguyen23hqv@users.noreply.github.com>
2020-11-29 22:08:18 +0700

    Update Latest.js

--- client/src/components/pages/Latest.js
+++ client/src/components/pages/Latest.js
@@ -6,91 +6,92 @@ class Latest extends React.Component {
     return (
       <Pane>
         <Pane
-          height={500}
-          width={615}
-          padding={170}
+          height={1000}
+          width={701}
+          float="left"
           style={{
-            backgroundColor: 'gray',
-            float: 'left',
-            textAlign: 'center',
+            backgroundColor: '#E4E7EB',
           }}
         >
-          <Button
-            width={200}
-            height={100}
-            style={{ textAlign: 'center', justifyContent: 'center' }}
-            appearance="primary"
+          <Pane
+            elevation={3}
+            height={120}
+            margin={32}
+            backgroundColor="white"
+            fontFamily="Arial"
           >
-            Bắt đầu học
+            <Pane float="left" paddingLeft={15}>
+              <img
+                src="https://thumbs.dreamstime.com/z/book-sign-book-symbol-vector-illustration-32931677.jpg"
+                width="100"
+                height="100"
+                alt="lul"
+              />
+            </Pane> 
+            <Pane
+              textAlign="center"
+              paddingTop={15}>
+              <p>Bạn đã tự tin với những gì mình được học, hãy <b>kiểm tra </b> nào.</p>
+              <Button appearance="primary"> Test</Button>
+            </Pane>
+          </Pane>
+          <Pane margin={34}>
+            <Pane
+              float="left"
+              fontSize={22}
+              style={{ fontFamily: 'Arial'}}>
+              Recent
+            </Pane>    
+            <Pane height={22} textAlign="right" 
+              fontSize={14}>
+              <a href="#"> Show all</a>
+            </Pane>   
+          </Pane> 
+          <Button 
+            float="left"
+            width={300}
+            height={120}
+            marginLeft={34}
+            marginBottom={15}
+            justifyContent="center"
+            alignItems="center"> 
+            <p>Bài 1</p>
           </Button>
+          <Button
+            width={300}
+            height={120}
+            marginLeft={34}
+            marginBottom={15}
+            justifyContent="center"
+            alignItems="center">
+            Bài 2
+          </Button>
+          <Button
+            float="left"
+            width={300}
+            height={120}
+            marginLeft={34}
+            marginBottom={15}
+            justifyContent="center"
+            alignItems="center">
+            Bài 3
+          </Button>
+          <Button 
+            width={300}
+            height={120}
+            marginLeft={34}
+            marginBottom={15}
+            justifyContent="center"
+            alignItems="center">Bài 4</Button>
         </Pane>
         <img
           style={{}}
           src="https://znews-photo.zadn.vn/w660/Uploaded/fcivbqmv/2019_06_15/gg_zing_2.jpg"
-          width="600"
-          height="500"
+          width="250"
+          height="1000"
           alt="lul"
         />
-        <Pane height={200} padding={50}>
-          <h1
-            style={{ fontFamily: 'Arial', fontSize: '20', textAlign: 'center' }}
-          >
-            90% người dùng Quizlet cho biết họ đã cải thiện được điểm số{' '}
-          </h1>
-        </Pane>
-        <Pane
-          height={400}
-          width={615}
-          padding={100}
-          style={{
-            fontFamily: 'Arial',
-            color: 'midnightblue',
-            backgroundColor: 'white',
-            float: 'left',
-            textAlign: 'center',
-          }}
-        >
-          <h2>Bạn chỉ cần động não, còn mọi thứ khác đã có chúng tôi lo </h2>
-        </Pane>
-        <img
-          src="https://znews-photo.zadn.vn/w660/Uploaded/fcivbqmv/2019_06_15/gg_zing_2.jpg"
-          width="600"
-          height="400"
-          alt="kekw"
-        />
-        <img
-          src="https://images.prismic.io/quizlet-prod/63a034d5-9f01-4024-a6bf-e36996575fd1_bannecker-benefit3.png?auto=compress,format"
-          width="600"
-          height="400"
-          alt="kapp"
-        />
-        <Pane
-          height={400}
-          width={615}
-          padding={100}
-          style={{
-            fontFamily: 'Arial',
-            color: 'midnightblue',
-            backgroundColor: 'white',
-            float: 'right',
-            textAlign: 'center',
-          }}
-        >
-          <h2>Đừng nản lòng. Cùng nỗ lực nào. </h2>
-        </Pane>
-        <Pane
-          height={60}
-          paddingTop={5}
-          paddingRight={30}
-          style={{
-            fontFamily: 'Arial',
-            backgroundColor: 'midnightblue',
-            color: 'white',
-            textAlign: 'right',
-          }}
-        >
-          <p>© 2020 Group 15 </p>
-        </Pane>
+
       </Pane>
     )
   }
